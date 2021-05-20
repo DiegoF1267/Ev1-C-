f55c618dffc6d7ffbe4e48fcdf093ff964dd81a6
Martin Polanco S <martinpolancosaavedra@gmail.com>
2021-05-20 19:28:20 -0400

    Se completo el proyecto

--- MedidoresApp/MedidoresApp/Hilos/HiloCliente.cs
+++ MedidoresApp/MedidoresApp/Hilos/HiloCliente.cs
@@ -1,4 +1,5 @@
-﻿using MedidoresModel.DAL;
+﻿using MedidoresApp.ValidacionesUtils;
+using MedidoresModel.DAL;
 using MedidoresModel.DTO;
 using SocketUtils;
 using System;
@@ -12,9 +13,7 @@ namespace MedidoresApp.Hilos
     class HiloCliente
     {
         private SocketCliente clienteSocket;
-        private IMedidorDAL dalMedidor = MedidorDALFactory.CreateDal();
-        private ILecturasDAL dalLectura = LecturasDALFactory.CreateDal();
-        static DateTime fechaActual = DateTime.Now;
+        private Validaciones v = new Validaciones();
 
         public HiloCliente(SocketCliente clienteSocket)
         {
@@ -32,223 +31,20 @@ namespace MedidoresApp.Hilos
                     mensaje = clienteSocket.Leer().Trim();
                 } while (mensaje == string.Empty);
 
-            } while (mensajeCorrecto(mensaje));
+            } while (v.mensajeCorrecto(mensaje));
 
             clienteSocket.Escribir(fechaActual.ToString("yyyy-MM-dd-HH-mm-ss") + "|WAIT");
 
             mensaje=clienteSocket.Leer().Trim();
 
-            lecturaCorrecta(mensaje);
+            mensaje=v.lecturaCorrecta(mensaje);
 
             clienteSocket.Escribir(mensaje);
 
+            clienteSocket.CerrarConexion();
+
         }
 
-        public void lecturaCorrecta(string mensaje)
-        {
-            int flag = 0;
-            int valor = 0;
-            string estado = "";
-            string tipo="";
-            DateTime fechaValida = new DateTime();
-            string[] mensajeArray = mensaje.Split('|');
-            if (mensajeArray.Length == 5 || mensajeArray.Length == 6)
-            {
-                if (existeMedidor(mensajeArray[0]))
-                {
-                    flag = flag + 1;
-                }
-
-               
-                try
-                {
-                    fechaValida = DateTime.Parse(mensajeArray[1]);
-                    if (fechaValida.ToString("yyyy-MM-dd-HH-mm-ss") == mensajeArray[1])
-                    {
-                        flag = flag + 1;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    flag = flag - 1;
-                }
-
-                if (existeTipo(mensajeArray[2]))
-                {
-                    flag = flag + 1;
-                    if (mensajeArray[2].Equals("consumo", StringComparison.OrdinalIgnoreCase))
-                    {
-                        tipo = "Consumo";
-                    }
-                    else
-                    {
-                        tipo = "Trafico";
-                    }
-                }
-
-                
-                try
-                {
-                    valor = Convert.ToInt32(mensajeArray[3]);
-                    if(valor >= 0 && valor <= 1000)
-                    {
-                        flag = flag + 1;
-                    }
-                }catch(Exception ex)
-                {
-                    flag = flag - 1;
-                }
-
-                
-                if(mensajeArray.Length == 6)
-                {
-                    //Viene con estado
-                    switch (mensajeArray[4])
-                    {
-                        case "-1": estado = "Error de Lectura";
-                            flag = flag + 1;
-                            break;
-                        case "0": estado = "OK";
-                            flag = flag + 1;
-                            break;
-                        case "1":
-                            estado = "Punto de carga lleno";
-                            flag = flag + 1;
-                            break;
-                        case "2":
-                            estado = "Requiere mantención preventiva";
-                            flag = flag + 1;
-                            break;
-                        default: flag = flag - 1;
-                            break;
-                    }
-
-                   if (mensajeArray[5].Equals("UPDATE"))
-                    {
-                        flag = flag + 1;
-                    }
-
-                }
-                else
-                {
-                    //Viene sin estado
-                    if (mensajeArray[4].Equals("UPDATE"))
-                    {
-                        flag = flag + 1;
-                    }
-                }
-               
-            }
-
-          
-
-
-            if ((mensajeArray.Length == 6 && flag==6) || (mensajeArray.Length == 5 && flag == 5))
-            {
-                Lectura l = new Lectura()
-                {
-                    Fecha = fechaValida,
-                    Valor = valor,
-                    Tipo = tipo,
-                    Estado = estado
-                };
-                    lock (dalLectura)
-                    {
-                        dalLectura.registrarLectura(l);
-                    }
-                mensaje = "";
-            }
-            else
-            {
-                mensaje = fechaActual.ToString("yyyy-MM-dd-HH-mm-ss")+"|"+mensajeArray[0]+"|ERROR";
-            }
-
-            
-        }
-
-        public bool mensajeCorrecto(string mensaje)
-        {
-            bool flag=false;
-            string fecha;
-            string nroMedidorStr;
-            string tipo;
-            string[] mensajeArray = mensaje.Split('|');
-            if (mensajeArray.Length == 3)
-            {
-                //Proceder con las demas verificaciones 
-                fecha = mensajeArray[0];
-                nroMedidorStr = mensajeArray[1];
-                tipo = mensajeArray[2];
-                
-                if (esFecha(fecha))
-                {
-                    if (existeMedidor(nroMedidorStr))
-                    {
-                        if (existeTipo(tipo))
-                        {
-                            flag = true;
-                        }
-                    }
-                }
-            }
-            return flag;
-        }
-
-        public bool esFecha (string fecha)
-        {
-            bool flag = false;
-            DateTime fechaValida = new DateTime();
-            try
-            {
-                fechaValida = DateTime.Parse(fecha);
-                if (fechaValida.ToString("yyyy-MM-dd-HH-mm-ss") == fecha)
-                {
-                    if ((fechaValida - fechaActual).Minutes < 30)
-                    {
-                        flag = true;
-                    }
-                }
-            }catch(Exception ex)
-            {
-                flag = false;
-            }
-
-            return flag;
-        }
-
-        public bool existeMedidor(string idMedidorStr)
-        {
-            bool flag = false;
-            int idMedidor=0;
-            try
-            {
-                idMedidor = Convert.ToInt32(idMedidorStr.Trim());
-                List<Medidor> medidores = dalMedidor.GetAll();
-                medidores.ForEach(m =>
-                {
-                    if (m.Id == idMedidor)
-                    {
-                        flag = true;
-                    }
-                });
-            }
-            catch(Exception ex)
-            {
-
-            }
-
-            
-            return flag;
-        }
-
-        public bool existeTipo(string tipo)
-        {
-            bool flag = false;
-            if(tipo.Equals("consumo", StringComparison.OrdinalIgnoreCase) || tipo.Equals("trafico", StringComparison.OrdinalIgnoreCase))
-            {
-                flag = true;
-            }
-            return flag;
-        }
+       
     }
 }
--- MedidoresApp/MedidoresApp/Parciales/Program.cs
+++ MedidoresApp/MedidoresApp/Parciales/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MedidoresApp.ValidacionesUtils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,23 +9,46 @@ namespace MedidoresApp
 {
     public partial class Program
     {
-       
+
+        public static void IngresarMensaje()
+        {
+            string mensaje;
+            DateTime fechaActual = DateTime.Now;
+            Validaciones v = new Validaciones();
+            do
+            {
+                do
+                {
+                    mensaje = Console.ReadLine().Trim();
+                } while (mensaje == string.Empty);
+
+            } while (v.mensajeCorrecto(mensaje));
+
+            Console.WriteLine(fechaActual.ToString("yyyy-MM-dd-HH-mm-ss") + "|WAIT");
+
+            mensaje = Console.ReadLine().Trim();
+
+            v.lecturaCorrecta(mensaje);
+
+            Console.WriteLine(mensaje);
+
+        }
 
         static bool menu()
         {
             bool flag = true;
-            Console.WriteLine("Seleccione una opcion");
-            Console.WriteLine("(1) Ingresar  ");
-            Console.WriteLine("(2) Mostrar ");
+            Console.WriteLine("Seleccione el tipo de medidor");
+            Console.WriteLine("(1) Trafico");
+            Console.WriteLine("(2) Consumo ");
             Console.WriteLine("(0) Salir");
             string respuesta = Console.ReadLine().Trim();
             switch (respuesta)
             {
                 case "1":
-                    ;
+                    IngresarMensaje();
                     break;
                 case "2":
-                    ;
+                    IngresarMensaje();
                     break;
                 case "0":
                     flag = false;
@@ -37,46 +61,5 @@ namespace MedidoresApp
             return flag;
         }
 
-         static void Medidor()
-        {
-            int nroMedidor = 0;
-
-            do
-            {
-                try
-                {
-                    Console.WriteLine("ingrese numero de medidor");
-                    nroMedidor = Int32.Parse(Console.ReadLine());
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("ingrese numero de medidor");
-
-                }
-            } while (nroMedidor >= 1 && nroMedidor <= 3);
-
-
-
-
-        }
-        static void tipo()
-        {
-            string tipo="";
-
-            do
-            {
-                try
-                {
-                    Console.WriteLine("ingrese tipo");
-                    tipo = Console.ReadLine();
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("ingrese tipo");
-                }
-            } while (tipo != "trafico" || tipo != "consumo");
-        }
     }
 }
